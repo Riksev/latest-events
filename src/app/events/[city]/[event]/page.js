8956fb44809da8ff5d9733b6ead5a5f9cec67d5e
Riksev <teraria1128@gmail.com>
2026-07-23 03:40:15 +0300

    feat(event): display event info + generateStaticParams

--- src/app/events/[city]/[event]/page.js
+++ src/app/events/[city]/[event]/page.js
@@ -1,12 +1,30 @@
-export default function EventPage() {
+import Image from "next/image";
+import data from "../../../../../data/data.json";
+
+export async function generateStaticParams() {
+  return data.allEvents.map((event) => ({
+    city: event.city.toLowerCase(),
+    event: event.id,
+  }));
+}
+
+export const dynamicParams = false;
+
+export default async function EventPage({ params }) {
+  const { city, event } = await params;
+  const { allEvents } = data;
+  const certainEvent = allEvents.find((ev) => ev.id === event);
   return (
     <div>
-      <h1>Event 1 in London</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quasi fugit
-        incidunt numquam. Sit porro earum eum, dicta natus recusandae aliquam
-        corrupti quas doloremque maxime similique, magni sunt neque soluta.
-      </p>
+      <h1>{certainEvent.title}</h1>
+      <h2>{certainEvent.city[0].toUpperCase() + certainEvent.city.slice(1)}</h2>
+      <Image
+        src={certainEvent.image}
+        alt={certainEvent.title}
+        width={430}
+        height={275}
+      />
+      <p>{certainEvent.description}</p>
     </div>
   );
 }
