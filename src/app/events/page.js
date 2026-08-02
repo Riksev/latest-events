dcd4f973fa7afaf39db64f025e945559c54e63d3
Riksev <teraria1128@gmail.com>
2026-08-03 02:07:26 +0300

    style: migrate CSS styles to Tailwind CSS

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,28 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 import data from "../../../data/data.json";
-import styles from ".././page.module.css";
 
 export default function EventsPage() {
-  const { events_categories } = data;
-  return (
-    <div className={styles.intro}>
-      <h1>Events</h1>
-      {events_categories.map((eventCategory) => (
-        <Link
-          href={`/events/${eventCategory.id}`}
-          className={styles.container}
-          key={eventCategory.id + "2"}
-        >
-          <Image
-            src={eventCategory.image}
-            alt={eventCategory.title}
-            width={500}
-            height={400}
-          />
-          <h2>{eventCategory.title}</h2>
-        </Link>
-      ))}
-    </div>
-  );
+    const { events_categories } = data;
+    return (
+        <div>
+            <h1>Events</h1>
+            {events_categories.map((eventCategory) => (
+                <Link
+                    href={`/events/${eventCategory.id}`}
+                    key={eventCategory.id + "2"}
+                >
+                    <Image
+                        src={eventCategory.image}
+                        alt={eventCategory.title}
+                        width={500}
+                        height={400}
+                    />
+                    <h2>{eventCategory.title}</h2>
+                </Link>
+            ))}
+        </div>
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,27 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
 import data from "../../data/data.json";
 
 export default function Home() {
-  const { events_categories } = data;
-  return (
-    <div className={styles.intro}>
-      <h1>Home</h1>
-      {events_categories.map((eventCategory) => (
-        <Link
-          href={`/events/${eventCategory.id}`}
-          className={styles.container}
-          key={eventCategory.id}
-        >
-          <Image
-            src={eventCategory.image}
-            alt={eventCategory.title}
-            width={400}
-            height={300}
-          />
-          <h2>{eventCategory.title}</h2>
-          <p>{eventCategory.description}</p>
-        </Link>
-      ))}
-    </div>
-  );
+    const { events_categories } = data;
+    return (
+        <div>
+            <h1>Home</h1>
+            {events_categories.map((eventCategory) => (
+                <Link
+                    href={`/events/${eventCategory.id}`}
+                    key={eventCategory.id}
+                >
+                    <Image
+                        src={eventCategory.image}
+                        alt={eventCategory.title}
+                        width={400}
+                        height={300}
+                    />
+                    <h2>{eventCategory.title}</h2>
+                    <p>{eventCategory.description}</p>
+                </Link>
+            ))}
+        </div>
+    );
 }
