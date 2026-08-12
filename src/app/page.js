0ab3426a3a1bb42a7ddec0de4b1f175618be0627
Riksev <teraria1128@gmail.com>
2026-08-13 00:10:49 +0300

    style(home): fix the nth-child problem

--- src/app/page.js
+++ src/app/page.js
@@ -7,27 +7,29 @@ export default function Home() {
     return (
         <div className="home-body">
             <h1>Home</h1>
-            {events_categories.map((eventCategory) => (
-                <Link
-                    href={`/events/${eventCategory.id}`}
-                    key={eventCategory.id}
-                    className="card"
-                >
-                    <Image
-                        src={eventCategory.image}
-                        alt={eventCategory.title}
-                        width={400}
-                        height={300}
-                        className="rounded-lg w-2/5"
-                    />
-                    <div className="flex flex-col">
-                        <h2 className="text-xl font-bold mb-2">
-                            {eventCategory.title}
-                        </h2>
-                        <p className="text-left">{eventCategory.description}</p>
-                    </div>
-                </Link>
-            ))}
+            <div className="cards">
+                {events_categories.map((eventCategory) => (
+                    <Link
+                        href={`/events/${eventCategory.id}`}
+                        key={eventCategory.id}
+                        className="card"
+                    >
+                        <Image
+                            src={eventCategory.image}
+                            alt={eventCategory.title}
+                            width={400}
+                            height={300}
+                            className="rounded-lg w-2/5"
+                        />
+                        <div className="flex flex-col">
+                            <h2 className="text-xl font-bold mb-2">
+                                {eventCategory.title}
+                            </h2>
+                            <p className="text-left">{eventCategory.description}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 }
